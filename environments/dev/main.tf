e6c97ea46f3cc2ad3048e6ca6447c92b5f200989
alorcoda <benjaminalornyeku@gmail.com>
2025-09-04 13:59:19 +0000

    dev modules deployment

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -5,17 +5,13 @@ terraform {
     bucket         = "benji-tf-state-bucket-3rd"
     key            = "dev/terraform.tfstate"
     region         = "us-west-2"
+    encrypt        = true 
     # use_lockfile = true 
   }
 }
 
 provider "aws" {
   region = var.aws_region
-}
-
-# Data source for VPC (if using existing VPC)
-data "aws_vpc" "default" {
-  default = true
 }
 
 # VPC module
@@ -32,10 +28,16 @@ module "vpc" {
 module "ec2" {
   source = "git::https://github.com/AlorCoda/terraform-aws-modules.git//modules/ec2?ref=main"
 
-  instance_type   = var.instance_type
-  ami             = var.ami_id
-  subnet_id       = module.vpc.public_subnet_ids[1]
-  security_groups = [module.sg.security_group_id]
+  # instance_type   = var.instance_type
+  # ami             = var.ami_id
+  # subnet_id       = module.vpc.public_subnet_ids[1]
+  # security_groups = [module.sg.security_group_id]
+  
+  instance_type    = var.instance_type
+  image_id        = data.aws_ami.ubuntu.id      # instead of 'ami'
+  subnet          = module.vpc.public_subnet_ids[0]  # instead of 'subnet_id'
+  security_group_ids = [aws_security_group.web_sg.id]  # instead of 'security_groups'
+
 }
 
 # Security Group module
@@ -47,30 +49,31 @@ module "sg" {
   vpc_id      = module.vpc.vpc_id
 }
 
-# # Data source for latest Ubuntu AMI
-# data "aws_ami" "ubuntu" {
-#   most_recent = true
+# Data source for latest Ubuntu AMI
+data "aws_ami" "ubuntu" {
+  most_recent = true
 
-#   filter {
-#     name   = "name"
-#     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-#   }
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
 
-#   filter {
-#     name   = "virtualization-type"
-#     values = ["hvm"]
-#   }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 
-#     filter {
-#     name   = "state"
-#     values = ["available"]
-#   }
+    filter {
+    name   = "state"
+    values = ["available"]
+  }
 
-#   filter {
-#     name   = "architecture"
-#     values = ["x86_64"]
-#   }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
 
-#   owners = ["099720109477"] # Canonical
-# }
+  owners = ["099720109477"] # Canonical
+}
+
 
--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -1,4 +1,4 @@
-output "cidr_block" {
+output "vpc_cidr_block" {
   description = "The CIDR block of the VPC"
   value = module.vpc.vpc_cidr_block
 }
