12972ad2b0ab76469d5607c56323a1ca5a216ab9
alorcoda <benjaminalornyeku@gmail.com>
2025-09-04 06:21:13 +0000

    dev modules deployment

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -7,16 +7,19 @@ variable "aws_region" {
 variable "cidr_block" {
   description = "CIDR block for the VPC"
   type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "public_subnets" {
   description = "Public subnets"
   type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "private_subnets" {
   description = "Private subnets"
   type        = string
+  default     = "10.0.3.0/24"
 }
 
 variable "instance_type" {
